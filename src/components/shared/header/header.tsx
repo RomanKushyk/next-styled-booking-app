0d5a7fa95d5fc43bed9f48073f239b5a5ebc2fb0
romankushyk <romankushyk0@gmail.com>
2025-11-14 19:37:37 +0200

    [TTT-1] feat: add header component and global styles with background

---
+++ src/components/shared/header/header.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "./header.styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Title>6037 Venture Partnership</Title>
+    </Container>
+  );
+}
