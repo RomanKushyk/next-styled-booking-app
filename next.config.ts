7588efd06ddbd8331492754d2ac68d17469f424b
romankushyk <romankushyk0@gmail.com>
2025-11-14 17:55:53 +0200

    Minor contig updates

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["localhost", "192.168.31.226"],
   reactCompiler: true,
   compiler: {
     styledComponents: {
