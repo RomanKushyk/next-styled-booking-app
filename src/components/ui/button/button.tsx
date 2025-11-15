6bfb212f04a9e5592864d6e3a318f43f9a6ed8ef
romankushyk <romankushyk0@gmail.com>
2025-11-15 14:17:42 +0200

    [TTT-3] feat: update the Confirm button component to handle active state and click events

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,11 +1,23 @@
+"use client";
 import React from "react";
 import { ButtonEl } from "./button.styles";
 
 interface Props {
   children: React.ReactNode;
   isActive: boolean;
+  onClick?: () => void;
 }
 
-export default function Button({ children, isActive = true }: Props) {
-  return <ButtonEl isActive={isActive}>{children}</ButtonEl>;
+export default function Button({ children, isActive = true, onClick }: Props) {
+  const handleClick = (): void => {
+    if (isActive) {
+      onClick?.();
+    }
+  };
+
+  return (
+    <ButtonEl $isActive={isActive} onClick={handleClick}>
+      {children}
+    </ButtonEl>
+  );
 }
