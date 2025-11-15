23d85083c800e59cf2642cb65737009164f5dcdd
romankushyk <romankushyk0@gmail.com>
2025-11-15 11:20:22 +0200

    [TTT-2] feat: add a booking card component base with a styled layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Container } from "./page.styles";
-import { Header } from "@/components/shared/header/header";
+import Header from "@/components/shared/header/header";
+import BookingCard from "@/components/shared/booking-card/booking-card";
 
 export default function Home() {
   return (
     <Container>
       <Header />
+
+      <BookingCard />
     </Container>
   );
 }
